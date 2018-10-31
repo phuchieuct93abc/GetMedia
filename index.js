84ad501cc29a6fca309790b3b9d272d6b3edc4df
hieu <phuchieuct93@gmail.com>
2018-10-31 22:30:23 +0700

    f

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var proxy = require('http-proxy-middleware');
 
 express()
   .use(express.static(path.join(__dirname, '')))
-  .use('/zingmp3', proxy({target: 'https://mp3.zing.vn', changeOrigin: false, pathRewrite: {
+  .use('/zingmp3', proxy({target: 'https://mp3.zing.vn', changeOrigin: true, pathRewrite: {
     '^/zingmp3' : '/',     // rewrite path
 },}))
   .get('/', (req, res) => res.render('index'))
